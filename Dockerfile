9e59a90857f1a955aa000f7dcf21496b77b9656c
axelaquino3 <axelaquino@google.com>
2024-06-24 22:09:13 -0400

    Created a Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:20
+COPY . /app
+
+WORKDIR /app
+
+RUN npm install && apt-get update && apt-get install -y sqlite3
+
+CMD [ "npm", "start" ]
+
+EXPOSE 3000
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const { Restaurant, Menu, Item } = require("../models/index")
-const db = require("../db/connection");
+const { check, validationResult } = require("express-validator")
+
 
 //TODO: Create your GET Request Route Below: 
 
@@ -11,6 +12,10 @@ app.use(express.json())
 // Helps parse the Request body with urlencoded()
 app.use(express.urlencoded({extended: true}))
 
+
+
+
+// READ - GET all restaurants
 app.get("/restaurants", async (request, response) => {
     const restaurants = await Restaurant.findAll({
         include: Menu,
@@ -25,8 +30,7 @@ app.get("/restaurants", async (request, response) => {
     response.send(restaurants)
 })
 
-
-// READ - GET
+// READ - GET single restuarant 
 app.get("/restaurants/:id", async (request, response) => {
     const id = request.params.id
     const restaurant = await Restaurant.findByPk(id)
@@ -35,9 +39,23 @@ app.get("/restaurants/:id", async (request, response) => {
 })
 
 // CREATE - POST
-app.post("/restaurants", async (request, response) => {
-    const newResaurant = await Restaurant.create(request.body)
-    response.json(newResaurant)
+app.post("/restaurants", [
+
+    check("name").not().isEmpty().trim(),
+    check("location").not().isEmpty().trim(),
+    check("cuisine").not().isEmpty().trim()
+
+], async (request, response) => {
+    
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        response.json({error: errors.array()})
+    } else {
+        const newResaurant = await Restaurant.create(request.body)
+        response.json(newResaurant)
+    }
+    
+   
 })
 
 // UPDATE - PUT/PATCH
@@ -50,7 +68,6 @@ app.put("/restaurants/:id", async (request, response) => {
     })
     response.json(updateRestaurant)
 })
-
 
 // DELETE - DESTROY
 app.delete("/restaurants/:id", async (request, response) => {
